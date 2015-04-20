86fffc709a7a1f33a408d22be859b328a40e3d59
Fernando <fernando.neto27051987@gmail.com>
2015-04-20 10:57:39 +0100

    remove var for dest

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ mysql -u root -e "USE 'aas_test';"
 mysql -u root -e "CREATE USER 'aas_test'@'localhost' IDENTIFIED BY 'aas_test'" 
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'aas_test'@'localhost' WITH GRANT OPTION;" 
 
-
-export STRIDER_CLONE_DEST="/tmp/"
 exec "$@"
 
 	
