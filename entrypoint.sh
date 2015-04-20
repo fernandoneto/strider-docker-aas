6a21992f88270276815b38eccaf5fb3ab77e36a2
Fernando <fernando.neto27051987@gmail.com>
2015-04-20 10:11:47 +0100

    add entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+service mysql start
+
+mysql -u root -e "CREATE DATABASE aas_test" 
+mysql -u root -e "USE 'aas_test';"
+mysql -u root -e "CREATE USER 'aas_test'@'localhost' IDENTIFIED BY 'aas_test'" 
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'aas_test'@'localhost' WITH GRANT OPTION;" 
+
+
+export STRIDER_CLONE_DEST="/tmp/"
+exec "$@"
+
+	
